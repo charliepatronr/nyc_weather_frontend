d7c2b80ed4002a72188248a20bf19359fead027d
charliepatronr <54086538+charliepatronr@users.noreply.github.com>
2021-03-17 00:29:11 -0600

    basic navigation complete

--- src/App.js
+++ src/App.js
@@ -1,112 +1,46 @@
 import React, {useState, useEffect} from "react"
+import LandingPage from './LandingPage'
+import CityWeather from './CityWeather'
+
 import {
-  GoogleMap,
-  useLoadScript,
-  Marker,
-  InfoWindow,
-} from "@react-google-maps/api";
-import mapStyles from "./mapStyles"
+  BrowserRouter, 
+  Switch, 
+  Route, 
+} from "react-router-dom"
 
 
-const libraries= ["places"];
-const mapContainerStyle = {
-  width: '100vw',
-  height: '100vh'
-}
-const center = {
-  lat: 40.714272, 
-  lng: -74.005966
-}
-const options = {
-  styles : mapStyles
-}
-
-
-export default function App () {
-
-
+function App() {
+    const url =  'http://localhost:3000/cities'
+    const [cities, setCities] = useState([])
   
+    useEffect( () => {
+      fetch(`${url}`)
+      .then(response => response.json())
+      .then(response => {
+        setCities([...response])
+        })
+    }, [])
 
-  const url =  'http://localhost:3000/cities'
-  const [cities, setCities] = useState([])
-  const [selected, setSelected] = useState(null)
-  // console.log(selected.lat, 'LAT')
-  // console.log(selected.lon, 'LON')
-
-
-  useEffect( () => {
-    fetch(`${url}`)
-    .then(response => response.json())
-    .then(response => {
-      console.log(typeof response)
-      let newState = [response]
-      console.log(newState)
-      setCities([...response])
-
-        // renderWishes(wishes)
-    })
-  }, [])
-
-
+    // create add and delete city function and pass it down ass props to landing page
   
-  // const renderMarkers = (cities) => {
+  return (
 
-  // }
+    <BrowserRouter>
+      <Switch>
+      <Route path='/weather/:id' component={CityWeather}/>
+        <Route path='/weather' 
+        render ={(props) =>(
+            <LandingPage {...props} cities={cities}/>
+        )}/>
+        <Route exact path='/' 
+        render ={(props) =>(
+            <LandingPage {...props} cities={cities}/>
+        )}/>
+        {/* ADD 404 PAGE  */}
+      </Switch>
+    </BrowserRouter>
 
-
-  const {isLoaded, loadError} = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries,
-  })
-
-  const mapRef = React.useRef();
-  const onMapLoad = React.useCallback((map) => {
-    mapRef.current = map;
-  }, []);
-
-
-  if(loadError) return "Error loading maps";
-  if(!isLoaded) return  "Loading Maps"
-  return(
-    <div>
-      <GoogleMap 
-      mapContainerStyle ={mapContainerStyle}
-      zoom={10}
-      center={center}
-      options = {options}
-      >
-      {
-        cities.map(element => (
-          <Marker 
-            key={element.city.weather_id} 
-            position ={{
-              lat: element.city.lat, 
-              lng: element.city.lon
-            }}
-            onClick = {() => {
-              setSelected(element.city)
-            }}
-            
-          />))
-        }
-
-      {selected ? (
-          <InfoWindow
-            position={{ lat: selected.lat, lng: selected.lon }}
-            onCloseClick={() => {
-              setSelected(null);
-            }}
-          >
-            <div>
-              <h2 >
-                {selected.name}
-              </h2>
-              <h2 onClick ={ () => console.log(selected)}> Expand </h2>
-            </div>
-          </InfoWindow>
-        ) : null}
-      </GoogleMap>
-    </div>
-
-  )
+  );
 }
+
+export default App;
