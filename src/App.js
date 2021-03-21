ac425e179efeea26814668b4e20c1abb44c70ac1
charliepatronr <54086538+charliepatronr@users.noreply.github.com>
2021-03-20 19:37:48 -0600

    updated to heroku api endpoint

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import {
 
 
 function App() {
-    const url =  'http://localhost:3000/cities'
+    const url =  'https://nyc-weather-frontend.herokuapp.com/cities'
     const [cities, setCities] = useState([])
   
     useEffect( () => {
